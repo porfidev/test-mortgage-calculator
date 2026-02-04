cf124fd3a9a4b4f179e023266c1d68f62ee623d5
porfi.dev <hola@porfi.dev>
2026-02-03 22:14:09 -0600

    feat: update styles

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  printWidth: 120,
+};
--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 function calculateGradient(rangeElement, textElement, rate) {
-    var currentVal = textElement.value = rangeElement.value;
-    var maxValue = rangeElement.getAttribute('max');
-    var percent = (currentVal * 100) / maxValue;
+    const currentVal = textElement.value = rangeElement.value;
+    const maxValue = rangeElement.getAttribute('max');
+    const percent = (currentVal * 100) / maxValue;
     rangeElement.style.background = 'linear-gradient(90deg, #1091cc ' + percent + '%, #d8d8d8 ' + percent + '%)';
     if (rate) {
-        textElement.value = currentVal / rate;
+        return textElement.value = currentVal / rate;
     }
 }
 
@@ -15,128 +15,91 @@ function preventTypeNonNumbers(event) {
 }
 
 function initializer() {
-    var rangeInput = document.getElementById('range');
-    var rangeTextInput = document.getElementById('range-text');
+  const rangeInput = document.getElementById('range');
+  const rangeTextInput = document.getElementById('range-text');
+  const rateInput = document.getElementById('rate');
+  const rateTextInput = document.getElementById('rate-text');
+  const textInputs = document.querySelectorAll('input[type=text]');
+  const form = document.getElementById('mc-form');
 
-    calculateGradient(rangeInput, rangeTextInput);
+  calculateGradient(rangeInput, rangeTextInput);
+  calculateGradient(rateInput, rateTextInput, 10);
 
-    rangeInput.addEventListener('input', function (event) {
-        calculateGradient(event.target, rangeTextInput, null)
-    }, false);
+  rangeInput.addEventListener('input', function (event) {
+      calculateGradient(event.target, rangeTextInput, null)
+  }, false);
 
+  rateInput.addEventListener('input', function (event) {
+      calculateGradient(event.target, rateTextInput, 10)
+  });
 
-    var rateInput = document.getElementById('rate');
-    var rateTextInput = document.getElementById('rate-text');
-
-    rateInput.addEventListener('input', function (event) {
-        calculateGradient(event.target, rateTextInput, 10)
+  textInputs.forEach(function (input) {
+    input.addEventListener('keypress', preventTypeNonNumbers);
+    input.addEventListener('paste', function (e) {
+      e.preventDefault()
     });
+  });
 
-    calculateGradient(rateInput, rateTextInput, 10);
-
-    var textInputs = document.querySelectorAll('input[type=text]');
-
-    textInputs.forEach(function (input) {
-        input.addEventListener('keypress', preventTypeNonNumbers);
-        input.addEventListener('paste', function (e) {
-            e.preventDefault()
-        });
-    });
-
-    var form = document.getElementById('mc-form');
-    form.addEventListener('submit', validateForm)
-}
-
-function animate(){
-    /** ANIMATION */
-    var resultDiv = document.getElementsByClassName('app-results-container');
-    resultDiv[0].classList.add('show');
-
-    var scrollSize = resultDiv[0].offsetHeight; // End Height of new document
-
-    var last = document.documentElement.scrollTop; // current position
-    var id = setInterval(function(){
-        if(last < scrollSize){
-            last += 5;
-            document.documentElement.scrollTo(0, last);
-        } else {
-            clearInterval(id);
-        }
-    }, 10);
+  form.addEventListener('submit', validateForm)
 }
 
 function calculateMorgage(form) {
+  const yearsOfMortgage = form.elements['range-text'].value;
+  const interestRate = form.elements['rate-text'].value;
+  const loanAmount = form.elements['loan'].value;
+  const annualTax = form.elements['tax'].value;
+  const annualInsurance = form.elements['insurance'].value;
 
-    var yearsOfMortgage = form.elements['range-text'].value;
-    var interestRate = form.elements['rate-text'].value;
-    var loanAmount = form.elements['loan'].value;
-    var annualTax = form.elements['tax'].value;
-    var annualInsurance = form.elements['insurance'].value;
+  const principleAndInterests = ((interestRate / 100) / 12) * loanAmount / (1 - Math.pow((1 + ((interestRate / 100) / 12)), -yearsOfMortgage * 12));
+  const tax = annualTax / 12;
+  const insurance = annualInsurance / 12;
+  const monthlyPayment = principleAndInterests + tax + insurance;
 
+  const principleElement = document.getElementById('principle-and-interests');
+  principleElement.innerText = principleAndInterests.toFixed(2);
+  principleElement.parentElement.classList.add('result');
 
-    // var principleAndInterests = ((interestRate / 100) / 12) * loanAmount / (1 - Math.pow((1 + ((interestRate / 100) / 12)), -yearsOfMortgage * 12));
-    var principleAndInterests = ((interestRate / 100) / 12) * loanAmount / (1 - Math.pow((1 + ((interestRate / 100) / 12)), -yearsOfMortgage * 12));
-    var tax = annualTax / 12;
-    var insurance = annualInsurance / 12;
-    var monthlyPayment = principleAndInterests + tax + insurance;
+  const taxResult = document.getElementById('tax-result');
+  taxResult.innerText = tax.toFixed(2);
+  taxResult.parentElement.classList.add('result');
 
-    var principleElement = document.getElementById('principle-and-interests');
-    principleElement.innerText = principleAndInterests.toFixed(2);
-    principleElement.parentElement.classList.add('result');
+  const insuranceResult = document.getElementById('insurance-result');
+  insuranceResult.innerText = insurance.toFixed(2);
+  insuranceResult.parentElement.classList.add('result');
 
-    var taxResult = document.getElementById('tax-result');
-    taxResult.innerText = tax.toFixed(2);
-    taxResult.parentElement.classList.add('result');
+  const monthyPaymentResult = document.getElementById('monthly-payment');
+  monthyPaymentResult.innerText = monthlyPayment.toFixed(2);
+  monthyPaymentResult.parentElement.classList.add('result');
 
-    var insuranceResult = document.getElementById('insurance-result');
-    insuranceResult.innerText = insurance.toFixed(2);
-    insuranceResult.parentElement.classList.add('result');
-
-    var monthyPaymentResult = document.getElementById('monthly-payment');
-    monthyPaymentResult.innerText = monthlyPayment.toFixed(2);
-    monthyPaymentResult.parentElement.classList.add('result');
-
-    form.querySelectorAll('button')[0].innerText = 'Recalculate';
-
-    animate();
-}
-
-function move(elem) {
-    var left = 0
-    function frame() {
-        left++  // update parameters
-        elem.style.left = left + 'px' // show frame
-        if (left == 100)  // check finish condition
-            clearInterval(id)
-    }
-    var id = setInterval(frame, 10) // draw every 10ms
+  form.querySelectorAll('button')[0].innerText = 'Recalculate';
 }
 
 function validateForm(event) {
     event.preventDefault();
-    var form = this;
-    var inputs = form.querySelectorAll('input');
-    var valid = true;
+    const form = this;
+    const inputs = Array.from(form.querySelectorAll('input'));
 
-    inputs.forEach(function (element, index) {
-        element.classList.remove('has-error');
-        if (element.nextElementSibling != null && element.nextElementSibling.classList.contains('helper')) {
-            element.nextElementSibling.style.display = 'none';
+    inputs.some(function (element) {
+      element.classList.remove('has-error');
+      if (element.nextElementSibling != null && element.nextElementSibling.classList.contains('helper')) {
+        element.nextElementSibling.style.display = 'none';
+      }
+
+      if (element.getAttribute('required') !== null) {
+        if (isNaN(parseFloat(element.value)) || element.value.trim().length === 0) {
+          element.classList.add('has-error');
+          if (element.nextElementSibling != null && element.nextElementSibling.classList.contains('helper')) {
+            element.nextElementSibling.style.display = 'block';
+          }
+
+          return true;
         }
-        if (element.getAttribute('required') !== null) {
-            if (isNaN(parseFloat(element.value)) || element.value.trim().length === 0) {
-                element.classList.add('has-error');
-                if (element.nextElementSibling != null && element.nextElementSibling.classList.contains('helper')) {
-                    element.nextElementSibling.style.display = 'block';
-                }
-                valid = false;
-            }
-        }
+      }
+
+      return false;
     });
 
-    if (valid) {
-        calculateMorgage(this);
-    }
+    calculateMorgage(this);
 }
 
 document.addEventListener('DOMContentLoaded', initializer);
